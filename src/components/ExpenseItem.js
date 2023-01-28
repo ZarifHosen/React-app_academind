95e5b1ec116949585952ea0d2bbc7d90a26792f4
Zarif Hosen <zarifhosen2015@gmail.com>
2023-01-28 19:37:51 +0600

    updated date

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,8 +1,12 @@
 import "./ExpenseItem.css";
+
 function ExpenseItem(props) {
+  const options = { month: "long", day: "numeric", year: "numeric" };
+  const formattedDate = props.date.toLocaleDateString("en-US", options);
+
   return (
     <div className="expense-item">
-      <div>{props.date.toDateString()}</div>
+      <div>{formattedDate}</div>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price"> ${props.amount} </div>
