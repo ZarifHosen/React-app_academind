be4fffd8c2304a1f5217f921a06a07eac2622100
Zarif Hosen <zarifhosen2015@gmail.com>
2023-01-31 14:29:14 +0600

    MUI integration in Buttons

--- src/components/Expense/ExpenseItem.js
+++ src/components/Expense/ExpenseItem.js
@@ -1,15 +1,32 @@
+import { useState } from "react";
+import * as React from "react";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 import Card from "../UI/Card";
+import Button from "@mui/material/Button";
 
 function ExpenseItem(props) {
+  const [title, setTitle] = useState(props.title);
+
+  const clickHandler = () => {
+    setTitle("Updated!");
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price"> ${props.amount} </div>
       </div>
+      <Button
+        variant="contained"
+        size="small"
+        color="secondary"
+        onClick={clickHandler}
+      >
+        change title
+      </Button>
     </Card>
   );
 }
