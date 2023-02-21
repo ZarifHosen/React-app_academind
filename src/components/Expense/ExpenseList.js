05108b89873080c52255adc66c4567fb79e5e824
Zarif Hosen <zarifhosen2015@gmail.com>
2023-02-21 17:21:03 +0600

    Year modal added

--- src/components/Expense/ExpenseList.js
+++ src/components/Expense/ExpenseList.js
@@ -1,20 +1,32 @@
+import {useState} from 'react';
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import "./ExpenseList.css";
+import ExpenseFilter from "./ExpensesFilter"
 
-function ExpenseList({ expenses }) {
-  return (
-    <Card className="expenses">
-      {expenses.map((expense) => (
-        <ExpenseItem
-          key={expense.id}
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-        />
-      ))}
-    </Card>
-  );
+function ExpenseList({expenses}) {
+    const [filteredYear, setFilteredYear] = useState('2020')
+
+    const filterChangeHandler = selectedYear => {
+        setFilteredYear(selectedYear)
+    }
+
+    return (
+        <div>
+            <Card className="expenses">
+                <ExpenseFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
+                {expenses.map((expense) => (
+                    <ExpenseItem
+                        key={expense.id}
+                        title={expense.title}
+                        amount={expense.amount}
+                        date={expense.date}
+                    />
+                ))}
+            </Card>
+
+        </div>
+    );
 }
 
 export default ExpenseList;
