a3a9c1a7c7c8061582b2d9055b3844bf3610eb07
Zarif Hosen <zarifhosen2015@gmail.com>
2023-02-25 22:37:28 +0600

    button removed

--- src/components/Expense/ExpenseItem.js
+++ src/components/Expense/ExpenseItem.js
@@ -1,32 +1,16 @@
-import { useState } from "react";
 import * as React from "react";
-import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 import Card from "../UI/Card";
-import Button from "@mui/material/Button";
+import ExpenseDate from "./ExpenseDate";
 
 function ExpenseItem(props) {
-  const [title, setTitle] = useState(props.title);
-
-  const clickHandler = () => {
-    setTitle("Updated!");
-  };
-
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price"> ${props.amount} </div>
       </div>
-      <Button
-        variant="contained"
-        size="small"
-        color="secondary"
-        onClick={clickHandler}
-      >
-        change title
-      </Button>
     </Card>
   );
 }
